2127723ca2b0bf218fd0524722f709ad8a5c1213
Pedro <88341822+PedroHenc@users.noreply.github.com>
2025-09-06 16:02:58 -0300

    adding audit to relatorios

--- src/main/java/com/bene/sgbr/model/Relatorio.java
+++ src/main/java/com/bene/sgbr/model/Relatorio.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -15,8 +16,14 @@ import java.io.Serializable;
 @Table(name = "relatorios")
 public class Relatorio implements Serializable {
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Audit audit;
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "EditedBy")
+    private String EditedBy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
